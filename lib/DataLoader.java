9eb0df504d8d17cb0b582e8defb79e3a6b9c4632
Foster Nicodin <fosternicodin@Fosters-MacBook-Pro.local>
2024-02-22 15:51:45 -0500

    Created DataLoader stubs  #23

--- lib/DataLoader.java
+++ lib/DataLoader.java
@@ -1,5 +1,16 @@
 package lib;
 
+import java.util.ArrayList;
+
 public class DataLoader {
     
+    public ArrayList<Course> getCourses() {
+        return new ArrayList<Course>;
+    }
+    public ArrayList<User> getUsers() {
+        return new ArrayList<User>:
+    }
+    public ArrayList<Major> getMajors() {
+        return new ArrayList<Major>;
+    }
 }
