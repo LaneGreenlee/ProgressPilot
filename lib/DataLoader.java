a6854053a9e1275cacb89abe4be53b3af8cdfb1d
Foster Nicodin <fosternicodin@Fosters-MacBook-Pro.local>
2024-02-22 13:58:19 -0500

    Created lib folder

---
+++ lib/DataLoader.java
@@ -0,0 +1,7 @@
+public abstract class DataLoader {
+	protected static final String PEOPLE_FILE_NAME = "people.json";
+	protected static final String PEOPLE = "people";
+	protected static final String PEOPLE_FIRST_NAME = "firstName";
+	protected static final String PEOPLE_LAST_NAME = "lastName";
+	protected static final String PEOPLE_PHONE_NUMBER = "phoneNumber";
+}
