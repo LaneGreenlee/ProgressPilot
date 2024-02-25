faf8cd688e60304715ca3b6df6c42a5d6db16370
LaneGreenlee <lanegreenlee9@gmail.com>
2024-02-25 00:21:33 -0500

    Create ProgressPilotFacade stubs #2

---
+++ lib/ProgessPilotFACADE.java
@@ -0,0 +1,27 @@
+package lib;
+
+import java.util.ArrayList;
+
+public class ProgessPilotFACADE {
+    private User user;
+
+    public User login(String userName, String password) {
+
+    }
+    public void logout() {
+
+    }
+    public boolean isLoggedIn() {
+
+    }
+    public ArrayList<Course> getAllCourses() {
+
+    }
+    public ArrayList<Major> getAllMajors() {
+
+    }
+    public ArrayList<Course> getCourseByMajor (Major major) {
+
+    }
+
+}
