e915e7cd241f4148bdde9d471b6fe3fefe9f8f22
Hani Rehmani <hanirehmani03@gmail.com>
2024-02-25 18:30:30 -0500

    fixed ProgressPilotFACADE.java Stubbs

--- lib/ProgessPilotFACADE.java
+++ lib/ProgessPilotFACADE.java
@@ -6,22 +6,22 @@ public class ProgessPilotFACADE {
     private User user;
 
     public User login(String userName, String password) {
-
+      return null;
     }
     public void logout() {
 
     }
     public boolean isLoggedIn() {
-
+      return false;
     }
     public ArrayList<Course> getAllCourses() {
-        
+      return null;
     }
     public ArrayList<Major> getAllMajors() {
-
+      return null;
     }
     public ArrayList<Course> getCourseByMajor (Major major) {
-
+      return null;
     }
 
 }
