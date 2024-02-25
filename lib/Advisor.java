b01a18b44280e5601a67bbe6bd771f6afa704851
LaneGreenlee <lanegreenlee9@gmail.com>
2024-02-25 00:16:17 -0500

    Create Advisor Class Stubs #22

---
+++ lib/Advisor.java
@@ -0,0 +1,52 @@
+package lib;
+import java.util.ArrayList;
+public class Advisor {
+    protected ArrayList<Student> students;
+
+    public Advisor(ArrayList<Student>) {
+
+    }
+
+    public String addNotes(String note, Student student ) {
+
+    }
+
+    protected void addStudent( Student student ) {
+
+    }
+
+    protected void removeStudent( Student student ) {
+
+    }
+
+    protected boolean checkIfFailing(Student student) {
+
+    }
+    protected void enterGrade (Student student, Course course, Grade grade) {
+
+    }
+    protected ArrayList<Student> viewStudents() {
+
+    }
+    protected Course viewCourse() {
+
+    }
+    protected void createCourse() {
+
+    }
+    protected void alterCourse(Course course) {
+
+    }
+    protected void deleteCourse(Course course) {
+
+    }
+    protected void createAccount(Student student) {
+
+    }
+    protected void  modifyAccount(Student student) {
+
+    }
+    protected void deleteAccount(Student student) {
+        
+    }
+}
