684b24fca0abde6aabcb3c32bb4665b3f011e7ac
Hani Rehmani <hanirehmani03@gmail.com>
2024-02-25 17:46:40 -0500

    Created Stubs for Student.java #20

---
+++ lib/Student.java
@@ -0,0 +1,50 @@
+package lib;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Student extends User{
+  private String gradYear;
+  private Major major;
+  private Double gpa;
+  private ArrayList<Scholarship> Scholarships;
+  private ArrayList<Course> failedCourses;
+  private ArrayList<Course> currentCourses;
+  public HashMap<Course, Grade> completedCourses;
+
+
+
+  public Student(String userName, String password, String firstName, String lastName, String uscID) {
+        super(userName, password, firstName, lastName, uscID);
+        //TODO Auto-generated constructor stub
+    }
+  protected void addEightSemesterPlan() {
+
+  }
+  protected boolean isFailing (Double Gpa) {
+    return false;
+  }
+  public double getGPA() {
+    return this.gpa;
+  }
+  public String FailedCourseWarning(Course failedcourses) {
+
+  } 
+  public String scholarshipWarning(double gpa) {
+
+  }
+  protected boolean meetsRequirements() {
+
+  }
+  protected void addCurrentCourse(Course course) {
+
+  }
+  public HashMap<Course, Grade> getCompletedCourses() {
+    return this.completedCourses;
+  }
+  public Major getMajor() {
+    return this.major;
+  }
+
+    
+}
