29df3a9d968a50fac30f504123c36c0efadeeb6b
Foster Nicodin <fosternicodin@Fosters-MacBook-Pro.local>
2024-02-24 22:11:04 -0500

    Created DataWriter stubs #4

---
+++ lib/DataWriter.java
@@ -0,0 +1,13 @@
+package lib;
+
+public class DataWriter {
+    public Boolean saveUsers() {
+        return true;
+    }
+    public Boolean saveCourses() {
+        return true;
+    }
+    public Boolean saveMajors() {
+        return true;
+    }
+}
