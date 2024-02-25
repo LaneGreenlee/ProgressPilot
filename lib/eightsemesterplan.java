66c5d0d3386c9b3caa205299ab906732f4b84adc
LaneGreenlee <lanegreenlee9@gmail.com>
2024-02-25 00:07:18 -0500

    Create EightSemester Class Stubs #12

---
+++ lib/eightsemesterplan.java
@@ -0,0 +1,49 @@
+package lib;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+public class eightsemesterplan {
+    private  ArrayList<Semester> semestersNeeded;
+    private Student student;
+    protected ArrayList<Course> whatIfCourses;
+    protected ArrayList<Course> majorMap;
+
+    public eightsemesterplan (Student student, takeCouses ArrayList<Couse>) {
+
+    }
+    public ArrayList<Course> getMajorMap (Major major) {
+
+    }
+public HashMap <Course, grade> completedMajorMap () {
+
+}
+
+protected void addSemester (Semester semester) {
+
+}
+protected void removeSemester (Semester semester) {
+
+}
+protected void savePlan(ArrayList<Course>) {
+
+}
+protected void deletePlan(ArrayList<Course) {
+
+}
+protected ArrayList<Course> getPlan (Student student) {
+
+}
+protected void printTranscript() {
+
+}
+public void whatIf (Student student, Major major) {
+
+}
+protected void createWhatIf (Student student, Major major) {
+
+}
+protected void deleteWhatIf (WhatIf whatIf) {
+
+}
+
+}
