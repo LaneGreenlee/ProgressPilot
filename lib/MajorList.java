d1e418c425888f0079f8082c08a11be7c18fb93d
Foster Nicodin <fosternicodin@Fosters-MacBook-Pro.local>
2024-02-24 22:07:29 -0500

    Created ApplicationArea stubs #3

--- lib/MajorList.java
+++ lib/MajorList.java
@@ -5,12 +5,13 @@ import java.util.ArrayList;
 public class MajorList {
     private ArrayList<Major> major;
     protected void addMajor (Double gpaRequirement, int totalHours, String college, MajorName majorName, ArrayList<Course> courses) {
-
+        Major newMajor = new Major(gpaRequirement, totalHours, college, majorName, courses);
+        major.add(newMajor);
     }
     protected void removeMajor (Major major) {
-        
+
     }
     protected MajorName getMajor (MajorName majorName) {
-        return null;
+        return majorName;
 }
 }
