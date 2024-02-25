a609de7f85f0fbc5ee47e53d0a40689706eb53cf
Hani Rehmani <hanirehmani03@gmail.com>
2024-02-25 17:17:54 -0500

    Create CourseClass Stubs #14

--- lib/Course.java
+++ lib/Course.java
@@ -1,5 +1,58 @@
 package lib;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class Course {
+    public String courseCode;
+    public String courseNumber;
+    public ArrayList<String> courseAttributes;
+    public String[] courseDescription;
+    public int creditHours;
+    public boolean isTransfer;
+    public UUID courseID;
+    private ArrayList<HashMap<Course, Double>> prereqs;
 
+    public Course(String courseCode, String courseNumber, String courseDescription, int creditHours) {
+    }
+
+    public boolean isAIU(Course course) {
+        return false;
+    }
+
+    public boolean isSCI(Course course) {
+        return false;
+    }
+
+    public boolean isGFL(Course course) {
+        return false;
+    }
+
+    public boolean isGHS(Course course) {
+        return false;
+    }
+
+    public boolean isGSS(Course course) {
+        return false;
+    }
+
+    public boolean isCMS(Course course) {
+        return false;
+    }
+
+    public boolean isINF(Course course) {
+        return false;
+    }
+
+    public boolean isFoundingDoc(Course course) {
+        return false;
+    }
+
+    public boolean isLiberalArts(Course course) {
+        return false;
+    }
+
+    public Course getPrereq(Course course) {
+        return null;
+    }
 }
