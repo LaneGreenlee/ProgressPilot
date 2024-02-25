c0510fdff85c8b783b535ff7eac779f912a275b9
Hani Rehmani <hanirehmani03@gmail.com>
2024-02-25 17:56:38 -0500

    Created Stubs for Semester.java #15

---
+++ lib/Semester.java
@@ -0,0 +1,28 @@
+package lib;
+import java.util.ArrayList;
+import java.util.UUID;;
+public class Semester {
+    public UUID semesterID;
+    public ArrayList<Course> coursesNeeded;
+    public int totalCreditHours;
+    public boolean isComplete;
+
+    public Semester(ArrayList<Course> courses) {
+
+    }
+    public void createPlan(Student student, ArrayList<Course> takeCourses) {
+
+    }
+    protected void addCourse(Course course) {
+
+    }
+    protected void removeCourse(Course course) {
+
+    }
+    protected void savePlan(ArrayList<Course> courses) {
+
+    }
+    protected void deletePlan(ArrayList<Course> courses) {
+
+    }
+}  
