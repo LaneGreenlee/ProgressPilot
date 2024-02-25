b1913d636e874e84b54ae86b231147d3ad1f7447
Hani Rehmani <hanirehmani03@gmail.com>
2024-02-25 17:24:22 -0500

    Completed Stubs for User.java #19

---
+++ lib/CourseList.java
@@ -0,0 +1,23 @@
+package lib;
+
+import java.util.ArrayList;
+
+public class CourseList {
+    public ArrayList<Course> Courses;
+    public CourseList() {
+        
+    }
+    protected void alterCourse(Course course) {
+
+    }
+    protected void deleteCourse(Course course) {
+
+    }
+    protected void createCourse(String courseCode, String courseNumber, String courseDescription, int creditHours) {
+
+    }
+    protected Course getCourse(String courseName) {
+        return null;
+    }
+
+}
---
+++ lib/User.java
@@ -0,0 +1,14 @@
+package lib;
+import java.util.UUID;
+
+public class User {
+    public String username;
+    public String password;
+    public String firstName;
+    public String lastName;
+    public UUID user_UUID;
+    protected String uscID;
+    protected User(String userName, String password, String firstName, String lastName, String uscID) {
+        
+    }
+}
